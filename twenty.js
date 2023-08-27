f89755700702025607e94dc45b94a8bc6c8e4f61
mohammed sulaiman <sulemanakhtar57@gmail.com>
2023-08-27 13:58:29 +0530

    3rd rest operator

--- twenty.js
+++ twenty.js
@@ -12,4 +12,13 @@ function add(a,b){
 }
 console.log(add(8,0))
 
+//3rd rest opera...
+
+function arrays(...rest){
+    for(let i of rest){
+        console.log(i)
+    }
+}
+arrays(10,11,12,13,14,15,16,17)
+
 
