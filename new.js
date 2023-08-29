c4f093408e12ece85eb8b6c5b9363a8f5ebe2c43
mohammed sulaiman <sulemanakhtar57@gmail.com>
2023-08-30 03:46:25 +0530

    callback

---
+++ new.js
@@ -0,0 +1,16 @@
+
+//callback function
+
+
+function main(){
+  console.log("hello")
+
+
+}
+function main2(a,b,callback){
+   console.log("hi...")
+   callback();
+   console.log(a+b)
+}
+main2(2,2,main)
+
